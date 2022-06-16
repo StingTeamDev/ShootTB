8c151a730e9851536500f758c8c8471bd3ba0b49
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-06-16 18:34:38 +0200

    Create Console.js

---
+++ Console.js
@@ -0,0 +1,4 @@
+console.log('%c ~> Name Temp : New Shoot 3alaenet Premium  ', 'background-color: yellow;padding: 2px 12px ; font-size: 12px;border-radius:5px;color:#000;font-weight:600');
+console.log('%c ~> Version : v1.2 Beta  ', 'background-color: yellow;padding: 2px 12px ; font-size: 12px;border-radius:5px;color:#000;font-weight:600');
+console.log('%c ~> Desgin URL : sting-web.blogspot.com  ', 'background-color: yellow;padding: 2px 12px ; font-size: 12px;border-radius:5px;color:#000;font-weight:600');
+console.log('%c ~> Dev By : Sting Dev Team  ', 'background-color: yellow;padding: 2px 12px ; font-size: 12px;border-radius:5px;color:#000;font-weight:600');
